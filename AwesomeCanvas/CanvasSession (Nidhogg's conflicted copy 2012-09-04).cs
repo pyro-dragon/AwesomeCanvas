3034497f80faa20f0462fb1628a2ac27e45c959c
Pyro-dragon <pyro@dragonscancode.com>
2012-09-04 22:43:10 +0100

    Made the WintabDN project into a .dll
    The .dll has been included in the AwesomeCanvas project.

---
+++ AwesomeCanvas/CanvasSession (Nidhogg's conflicted copy 2012-09-04).cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using AwesomeCanvas;
+using System.IO;
+using WintabDN;
+namespace AwesomeCanvas
+{
+    /// <summary>
+    ///  this class collects input from the gui
+    ///  it also handles the controllers associated with the canvas
+    /// </summary>
+    public class CanvasSession
+    {
+        ToolRunner m_toolRunner;
+        CanvasWindow m_canvasWindow;
+        MainForm m_mainForm;
+        LayerControlForm m_layerControl;
+        
+        public CanvasWindow canvasWindow { get { return m_canvasWindow; } }
+        public ToolRunner localController { get { return m_toolRunner; } }
+        //-------------------------------------------------------------------------
+        // Constructor
+        //-------------------------------------------------------------------------
+        public CanvasSession(MainForm pMainForm, CanvasWindow pCanvasWindow, LayerControlForm pLayerControlForm)
+        {
+            m_toolRunner = new ToolRunner("localUserInput", pCanvasWindow.GetPicture());
+            m_mainForm = pMainForm;
+            m_canvasWindow = pCanvasWindow;
+            m_canvasWindow.m_session = this;
+            m_layerControl = pLayerControlForm;
+            
+            //setup first layer
+            selectedLayerID = Gui_CreateLayer();
+            Gui_ClearSelectedLayer();
+            
+            //add listeners for all functions that should redraw the main canvas
+            m_toolRunner.AddFunctionListener( (pA, pB, pC) => { m_canvasWindow.Redraw(pA); }, "tool_down", "tool_up", "tool_move", "undo", "clear", "reorder_layers", "remove_layer");
+
+            //add listeners for all functions that should rebuild the layer list
+            m_toolRunner.AddFunctionListener((pA, pB, pC) => { m_layerControl.RebuildLayerControls(); }, "reorder_layers", "rename_layer", "remove_layer", "create_layer");
+            
+            //add listeners for all functions that should update a layer thumbnail
+            m_toolRunner.AddFunctionListener((pA, pB, pC) => { m_layerControl.UpdateThumbnail(pC["layer"] as string); }, "tool_up", "undo", "clear");
+            
+            //add listeners for updating the status bar (:
+            m_toolRunner.AddFunctionListener((pA, pB, pC) => { m_mainForm.SetStatus("last action: " + pB); }, "tool_down", "tool_up", "tool_move", "undo", "clear", "reorder_layers", "remove_layer", "create_layer");
+        }
+
+        internal void GuiInput_MouseUp(object sender, MouseEventArgs e)
+        {
+            EzJson j = new EzJson();
+            j.BeginFunction("tool_up");
+            j.AddData("x", (int)(e.X / m_canvasWindow.magnification));
+            j.AddData("y", (int)(e.Y / m_canvasWindow.magnification));
+            j.AddData("pressure", 0);
+            j.AddData("layer", selectedLayerID);
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void GuiInput_TabletUp(object sender, WintabPacket pkt)
+        {
+            EzJson j = new EzJson();
+            j.BeginFunction("tool_up");
+            j.AddData("x", (int)(pkt.pkX / m_canvasWindow.magnification));
+            j.AddData("y", (int)(pkt.pkY / m_canvasWindow.magnification));
+            j.AddData("pressure", pkt.pkNormalPressure.pkAbsoluteNormalPressure.ToString());
+            j.AddData("layer", selectedLayerID);
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void GuiInput_MouseDown(object sender, MouseEventArgs e)
+        {
+            string toolName = m_mainForm.GetToolName();
+            EzJson j = new EzJson();
+            j.BeginFunction("tool_down");
+            j.AddData("pressure", (128).ToString());
+            j.AddData("x", (int)(e.X / m_canvasWindow.magnification));
+            j.AddData("y", (int)(e.Y / m_canvasWindow.magnification));
+            j.AddData("layer", selectedLayerID);
+            j.AddData("tool", toolName);
+            switch (toolName) {
+                case "brush":
+                    j.AddData("options", m_mainForm.GetBrushOptions());
+                break;
+                case "pen":
+                    j.AddData("options", m_mainForm.GetPenOptions());
+                break;
+                default:
+                    j.AddData("options", null);
+                break;
+            }
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void GuiInput_TabletDown(object sender, WintabPacket pkt)
+        {
+            string toolName = m_mainForm.GetToolName();
+            EzJson j = new EzJson();
+            j.BeginFunction("tool_down");
+            j.AddData("pressure", pkt.pkNormalPressure.pkAbsoluteNormalPressure.ToString());
+            j.AddData("x", (int)(pkt.pkX / m_canvasWindow.magnification));
+            j.AddData("y", (int)(pkt.pkY / m_canvasWindow.magnification));
+            j.AddData("layer", selectedLayerID);
+            j.AddData("tool", toolName);
+            switch (toolName)
+            {
+                case "brush":
+                    j.AddData("options", m_mainForm.GetBrushOptions());
+                    break;
+                case "pen":
+                    j.AddData("options", m_mainForm.GetPenOptions());
+                    break;
+                default:
+                    j.AddData("options", null);
+                    break;
+            }
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void GuiInput_MouseMove(object sender, MouseEventArgs e)
+        {
+            EzJson j = new EzJson();
+            j.BeginFunction("tool_move");
+            j.AddData("pressure", (128).ToString());
+            j.AddData("x", (int)(e.X / m_canvasWindow.magnification));
+            j.AddData("y", (int)(e.Y / m_canvasWindow.magnification));
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void GuiInput_TabletMove(object sender, WintabPacket pkt)
+        {
+            EzJson j = new EzJson();
+            j.BeginFunction("tool_move");
+            j.AddData("pressure", pkt.pkNormalPressure.pkAbsoluteNormalPressure.ToString());
+            j.AddData("x", (int)(pkt.pkX / m_canvasWindow.magnification));
+            j.AddData("y", (int)(pkt.pkY / m_canvasWindow.magnification));
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void Gui_Undo() {
+            EzJson j = new EzJson();
+            j.BeginFunction("undo");
+            j.AddData("layer", selectedLayerID);
+            m_toolRunner.ParseJSON(j.Finish());
+            Console.WriteLine("undo!");
+        }
+
+        internal void Gui_ClearSelectedLayer() {
+            EzJson j = new EzJson();
+            j.BeginFunction("clear");
+            j.AddData("layer", selectedLayerID);
+            m_toolRunner.ParseJSON(j.Finish());
+
+            Console.WriteLine("clear!");
+        }
+        internal void Gui_RenameLayer(string pLayerID, string pNewName) {
+            EzJson j = new EzJson();
+            j.BeginFunction("rename_layer");
+            j.AddData("layer", pLayerID);
+            j.AddData("name", pNewName);
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal string Gui_CreateLayer() {
+            string id = Guid.NewGuid().ToString(); //create a globally unique id
+            EzJson j = new EzJson();
+            j.BeginFunction("create_layer");
+            j.AddData("layer", id);
+            m_toolRunner.ParseJSON(j.Finish());
+            return id;
+        }
+        internal void Gui_RemoveLayer( string pLayerID) {
+            EzJson j = new EzJson();
+            j.BeginFunction("remove_layer");
+            j.AddData("layer", pLayerID);
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+        internal void Gui_SetLayerOrder(string[] pOrderedIDs) {
+            EzJson j = new EzJson();
+            j.BeginFunction("reorder_layers");
+            j.AddData("order", pOrderedIDs);
+            m_toolRunner.ParseJSON(j.Finish());
+        }
+
+        internal void SaveCanvasToFile(string pFileName) 
+        {
+            using (StreamWriter newTask = new StreamWriter(pFileName, false)) {
+                
+            }
+            Console.WriteLine("trying to save file: " + pFileName);
+        
+        }
+
+        internal Picture GetPicture() {
+            return m_canvasWindow.GetPicture();
+        }
+
+
+
+        public string selectedLayerID { get; set; }
+    }
+}
---
+++ AwesomeCanvas/Forms/CanvasWindow (Nidhogg's conflicted copy 2012-09-03).cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WintabDN;
+
+namespace AwesomeCanvas
+{
+    //public delegate void CanvasMouseEvent(CanvasWindow window, MouseEventArgs args);
+
+    //-------------------------------------------------------------------------
+    // The form that contains a picture and displays it
+    //-------------------------------------------------------------------------
+    public partial class CanvasWindow : Form
+    {
+        // Member variables
+        float m_magnification = 1.0f;
+        Picture m_picture;
+        string m_pictureName;
+        PanTool m_panTool;
+        ZoomTool m_zoomTool;
+        
+        internal CanvasSession m_session;
+
+         // Experimental tablet shit
+        CWintabData data;
+        int lastPressure;
+        
+
+        public float magnification { get { return m_magnification; } }
+
+        //---------------------------------------------------------------------
+        // Constructor
+        //---------------------------------------------------------------------
+        public CanvasWindow(int width = 200, int height = 200, string name = "New Picture")
+        {
+            InitializeComponent();
+            this.Text = name;
+            m_picture = new Picture(width, height, name);
+            this.SetClientSizeCore((int)(m_picture.Width), (int)(m_picture.Height));
+            
+            m_panTool = new PanTool(this);
+            m_zoomTool = new ZoomTool(this);
+
+            SetZoom(1.0f, false);
+            this.canvasBox.MouseDown += new MouseEventHandler(ProcessMouseDown);  
+            this.canvasBox.MouseUp += new MouseEventHandler(ProcessMouseUp); 
+            this.canvasBox.MouseMove += new MouseEventHandler(ProcessMouseMove);
+            this.WindowState = FormWindowState.Maximized;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.ControlBox = false;
+            //this.KeyDown += (object a, KeyEventArgs b) => ProcessKeyDown(b.KeyData);
+            //this.KeyUp += (object a, KeyEventArgs b) => ProcessKeyUp(b.KeyData);
+        }
+        //---------------------------------------------------------------------
+        // Set the zoom level of the image
+        //---------------------------------------------------------------------
+        public void SetZoom(float pMaginfication, bool pForceRedraw) {
+            m_magnification = pMaginfication;
+            canvasBox.Size = this.AutoScrollMinSize = new Size((int)(m_picture.Width * m_magnification), (int)(m_picture.Height * m_magnification));   // Make the window add scroll bars if nesasery
+            if (pForceRedraw)
+                Redraw(null);
+        }
+
+        //---------------------------------------------------------------------
+        // Return the picture that is being used
+        //---------------------------------------------------------------------
+        public Picture GetPicture()
+        {
+            return m_picture;
+        }
+
+        //---------------------------------------------------------------------
+        // Draw the canvas
+        //---------------------------------------------------------------------
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle sampleRectagle = e.ClipRectangle;
+            sampleRectagle.X = (int)(sampleRectagle.X / magnification);
+            sampleRectagle.Y = (int)(sampleRectagle.Y / magnification);
+            sampleRectagle.Width = (int)(sampleRectagle.Width / m_magnification);
+            sampleRectagle.Height = (int)(sampleRectagle.Height / m_magnification);
+            Rectangle targetRectangle = e.ClipRectangle;
+            m_picture.DrawPicture(e.Graphics,targetRectangle, sampleRectagle);
+        }
+
+        public void Redraw( ToolRunner pController ) 
+        {
+            canvasBox.Invalidate(true);
+            canvasBox.Update();
+         
+        }
+        internal void ProcessMouseUp(object sender, MouseEventArgs e) {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left) {
+                m_session.GuiInput_MouseUp(sender, e);
+            }
+        }
+
+        internal void ProcessMouseDown(object sender, MouseEventArgs e) 
+        {
+            
+            if (e.Button == System.Windows.Forms.MouseButtons.Left) 
+            {
+                if (m_zoomTool.Enabled) {
+                    m_zoomTool.Begin(new Point(e.X, e.Y));
+                }
+                else if (m_panTool.Enabled) {
+                    m_panTool.Begin(new Point(e.X, e.Y));
+                }
+          /*      else if ((Control.ModifierKeys & Keys.Control) != Keys.None) {
+                    SetZoom(m_magnification / 1.5f, true);
+                }
+                else if ((Control.ModifierKeys & Keys.Shift) != Keys.None) {
+                    SetZoom(m_magnification * 1.5f, true);
+                }*/
+                else {
+                    m_session.GuiInput_MouseDown(sender, e);
+                }
+            }
+        }
+
+        internal void ProcessMouseMove(object sender, MouseEventArgs e) {
+            //if left button is down
+
+            if (m_zoomTool.Enabled && !Control.ModifierKeys.HasFlag(Keys.Shift))
+                m_zoomTool.Enabled = false;
+
+            if (m_panTool.Enabled && !Control.ModifierKeys.HasFlag(Keys.Alt))
+                m_panTool.Enabled = false;
+       
+            if ((Control.MouseButtons & System.Windows.Forms.MouseButtons.Left) != System.Windows.Forms.MouseButtons.None) 
+            {
+                if (m_zoomTool.Enabled) {
+                    m_zoomTool.Move(new Point(e.X, e.Y));
+                    
+                }
+                else if (m_panTool.Enabled) {
+                    m_panTool.Move(new Point(e.X, e.Y));
+                }
+                else {
+                    m_session.GuiInput_MouseMove(sender, e);
+                }
+            }
+            
+        }
+
+        internal bool ProcessKeyDown(Keys pKeys)
+        {
+            if(pKeys == (Keys.Control | Keys.X))
+                m_session.Gui_ClearSelectedLayer();
+            else if(pKeys == (Keys.Control | Keys.Z))
+                m_session.Gui_Undo();
+            else if (pKeys.HasFlag(Keys.Shift))
+                m_zoomTool.Enabled = true;
+            else if (pKeys.HasFlag(Keys.Alt))
+                m_panTool.Enabled = true;
+            else if (pKeys.HasFlag(Keys.Oemplus))
+                SetZoom(m_magnification * 1.5f, true);
+            else if (pKeys.HasFlag(Keys.OemMinus))
+                SetZoom(m_magnification / 1.5f, true);
+             else 
+                return false;
+            return true;
+           
+        }
+        internal void SetPanPosition(Point pPoint) {
+            this.SetDisplayRectLocation(pPoint.X, pPoint.Y);
+            Redraw(null);
+            //this.Rect
+            //this.VerticalScroll.Value = Math.Max(0, pPoint.Y);
+            //this.HorizontalScroll.Value =  Math.Max(0, pPoint.X);
+        }
+        internal Point GetPanPosition() {
+            return new Point(this.DisplayRectangle.X, this.DisplayRectangle.Y);
+        }
+
+        //---------------------------------------------------------------------
+        // Prepare to take tablet input
+        //---------------------------------------------------------------------
+        private void PrepareForTabletInput(object sender, EventArgs e)
+        {
+            // Set up the tablet context
+            CWintabContext context = new CWintabContext();
+            context = CWintabInfo.GetDefaultDigitizingContext(ECTXOptionValues.CXO_MESSAGES);
+
+            // Allow the mouse to move
+            context.Options |= (uint)ECTXOptionValues.CXO_SYSTEM;
+
+            context.Name = "Tablet event data context";
+            context.OutOrgX = 0;
+            context.OutOrgY = 0;
+            context.OutExtX = 1000;
+            context.OutExtY = 1000;
+
+            bool status = context.Open();
+
+            // Prepare to take data
+            data = new CWintabData(context);
+            data.SetWTPacketEventHandler(TabletEventHandler);
+        }
+
+        //---------------------------------------------------------------------
+        // Tablet event handler
+        //---------------------------------------------------------------------
+        private void TabletEventHandler(object sender, MessageReceivedEventArgs args)
+        {
+            uint pktID = (uint)args.Message.WParam;
+            WintabPacket pkt = data.GetDataPacket(pktID);
+
+            if (pkt.pkNormalPressure.pkAbsoluteNormalPressure > 0)
+            {
+                if (lastPressure > 0)
+                    m_session.GuiInput_TabletMove(sender, pkt);
+                else
+                    m_session.GuiInput_TabletDown(sender, pkt);
+            }
+            else if (pkt.pkNormalPressure.pkAbsoluteNormalPressure == 0 && lastPressure > 0)
+                m_session.GuiInput_TabletUp(sender, pkt);
+        }
+    }
+}
